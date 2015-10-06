28816ca07ad939aad6bdeba361ea5835ef7a0b61
timbog <timbog@mail.ru>
2015-10-06 14:08:09 +0300

    Architecture impoved

--- ExploreBaseControls/CrudeOilClient.cs
+++ ExploreBaseControls/CrudeOilClient.cs
@@ -10,57 +10,27 @@ using Microsoft.Phone.Controls;
 
 namespace ExploreBaseControls
 {
-    public class CrudeOilClient
+    public class CrudeOilClient:Client
     {
-        private static string[] codes = { "CL1:COM", "CO1:COM" };
         private const string url = "http://www.bloomberg.com/energy";
-        private string html { get; set; }
-        private MainPage page;
 
-        public CrudeOilClient(MainPage page)
+        protected override String GetUrl()
         {
-            this.page = page;
-            UpdateInfo();
+            return url;
         }
 
-        public void UpdateInfo()
+        protected override int GetLength()
         {
-            HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(url);
-            request.BeginGetResponse(GetData, request);
+            return 68081;
         }
 
-        public void GetData(IAsyncResult result)
+        protected override void UpdatePage()
         {
-            HttpWebRequest request = result.AsyncState as HttpWebRequest;
-            if (request != null)
-            {
-                try
-                {
-                    WebResponse response = request.EndGetResponse(result);
-                    MemoryStream stream = new MemoryStream();
-                    response.GetResponseStream().CopyTo(stream);
-                    StreamReader reader = new StreamReader(stream);
-                    html = Encoding.UTF8.GetString(stream.ToArray(), 0, 68081);
-                    page.UpdateOil();
-                }
-                catch (WebException e)
-                {
-                    return;
-                }
-            }
+            page.UpdateOil();
         }
 
-        public float Parse(String code)
+        public CrudeOilClient(MainPage page) : base(page)
         {
-            int a = html.Length;
-            int Ind = html.IndexOf(code);
-            string substr = html.Substring(Ind);
-            int valueStartInd = substr.IndexOf("value") + 7;
-            int valueEndInd = substr.IndexOf('<', valueStartInd);
-            string substr2 = substr.Substring(valueStartInd, valueEndInd - valueStartInd);
-            float f;
-            float.TryParse(substr2, NumberStyles.Any, new CultureInfo("en-US"), out f);
-            return f;
         }
     }
 }
--- ExploreBaseControls/CurrencyClient.cs
+++ ExploreBaseControls/CurrencyClient.cs
@@ -11,57 +11,27 @@ using Microsoft.Phone.Controls;
 
 namespace ExploreBaseControls
 {
-    public class CurrencyClient
+    public class CurrencyClient:Client
     {
-        private static string[] codes = { "USD-JPY", "EUR-USD", "RUB", "USD-CHF", "GBP-USD" };
         private const string url = "http://www.bloomberg.com/markets/currencies";
-        private string html {get; set;}
-        private MainPage page;
 
-        public CurrencyClient(MainPage page)
+        protected override String GetUrl()
         {
-            this.page = page;
-            UpdateInfo();
+            return url;
         }
 
-        public void UpdateInfo()
+        protected override int GetLength()
         {
-            HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(url);
-            request.BeginGetResponse(GetData, request);
+            return 123763;
         }
 
-        public void GetData(IAsyncResult result)
+        protected override void UpdatePage()
         {
-            HttpWebRequest request = result.AsyncState as HttpWebRequest;
-            if (request != null)
-            {
-                try
-                {
-                    WebResponse response = request.EndGetResponse(result);
-                    MemoryStream stream = new MemoryStream();
-                    response.GetResponseStream().CopyTo(stream);
-                    StreamReader reader = new StreamReader(stream);
-                    html = System.Text.Encoding.UTF8.GetString(stream.ToArray(), 0, 123763);
-                    page.UpdateCurrency();
-                }
-                catch (WebException e)
-                {
-                    return;
-                }
-            }
+            page.UpdateCurrency();
         }
 
-        public float Parse(String code)
+        public CurrencyClient(MainPage page): base(page)
         {
-            int a = html.Length;
-            int rubleInd = html.IndexOf(code);
-            string substr = html.Substring(rubleInd);
-            int valueStartInd = substr.IndexOf("value") + 7;
-            int valueEndInd = substr.IndexOf('<', valueStartInd);
-            string substr2 = substr.Substring(valueStartInd, valueEndInd - valueStartInd);
-            float f;
-            float.TryParse(substr2, NumberStyles.Any, new CultureInfo("en-US"), out f);
-            return f;
         }
     }
 }
