a45bb781d285d59bf3c9f190d375993acfb981a5
timbog <timbog@mail.ru>
2015-10-08 15:29:38 +0300

    files added

---
+++ ExploreBaseControls/Client.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+using System.Globalization;
+
+namespace ExploreBaseControls
+{
+    public abstract class Client
+    {
+        protected MainPage page;
+        protected string html { get; set; }
+
+        protected abstract String GetUrl();
+
+        protected abstract void UpdatePage();
+
+        protected abstract int GetLength();
+
+        public Client(MainPage page)
+        {
+            this.page = page;
+            UpdateInfo();
+        }
+
+        public void UpdateInfo()
+        {
+            HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(GetUrl());
+            request.BeginGetResponse(GetData, request);
+        }
+
+        public void GetData(IAsyncResult result) {
+            HttpWebRequest request = result.AsyncState as HttpWebRequest;
+            if (request != null)
+            {
+                try
+                {
+                    WebResponse response = request.EndGetResponse(result);
+                    MemoryStream stream = new MemoryStream();
+                    response.GetResponseStream().CopyTo(stream);
+                    StreamReader reader = new StreamReader(stream);
+                    html = Encoding.UTF8.GetString(stream.ToArray(), 0, GetLength());
+                    UpdatePage();
+                }
+                catch (WebException e)
+                {
+                    return;
+                }
+            }
+        }
+
+        public float Parse(String code)
+        {
+            int a = html.Length;
+            int rubleInd = html.IndexOf(code);
+            string substr = html.Substring(rubleInd);
+            int valueStartInd = substr.IndexOf("value") + 7;
+            int valueEndInd = substr.IndexOf('<', valueStartInd);
+            string substr2 = substr.Substring(valueStartInd, valueEndInd - valueStartInd);
+            float f;
+            float.TryParse(substr2, NumberStyles.Any, new CultureInfo("en-US"), out f);
+            return f;
+        }
+
+    }
+}
